ed871dd82a4c1e5669a0bc3c1cdbbcba7ba1e819
Pedro Henrique Miho de Souza <pedromihosoza@gmail.com>
2025-02-17 10:38:14 -0300

    Aula 02 - Variaveis

---
+++ Aula 02 - Variaveis/src/Variaveis/OperadoresAritmeticos.java
@@ -0,0 +1,27 @@
+package Variaveis;
+import java.util.Scanner;
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		String nome;
+		System.out.println("Digite seu nome: ");
+		nome = sc.next(); // Armazenar valores do tipo texto
+		System.out.println(nome);
+		
+		System.out.println("Digite a sua idade: ");
+		int idade = sc.nextInt(); // Armazenar valores do tipo inteiro
+		
+		System.out.println("Digite seu peso: ");
+		double peso = sc.nextDouble(); // Armazenar valores do tipo flutuante (Decimal)
+		
+		System.out.println("A sua idade é de " + idade + " anos");
+		System.out.println("O seu peso é de " + peso + " kg");
+		
+		sc.close();
+	}
+
+}
---
+++ Aula 02 - Variaveis/src/Variaveis/TiposDados.java
@@ -0,0 +1,35 @@
+package Variaveis;
+
+public class TiposDados {
+
+	public static void main(String[] args) {
+		// Tipos Inteiros
+		// byte = - 128 - 127
+		byte numero = 127;
+		
+		// short  = -32.768 - 32.767
+		short n2 = 300;
+		
+		// int = -2.147.483.648 - 2.147.483.647 
+		int idade = 40;
+		
+		//long 
+		long n3 = 500;
+		
+		// Tipos Decimais
+		//float = -3,4 * 10^38 - 3,4 * 10^38 
+		float peso = 30.60f ;
+		//double -1,8 * 10 ^ 308 - 1,8 * 10 ^ 308
+		double altura = 1.70;
+		
+		//Tipos Texto
+		// "" - String
+		// '' - char
+		char genero = 'F';
+		String nome = "Pedro";
+		
+		//Tipo booleano
+		boolean logico = true;
+	}
+
+}
