c4673cb548d1921cd8fe41c88f875b5948ad6c6d
PedroMiho <pedromihosoza@gmail.com>
2025-04-16 10:37:16 -0300

    .

--- Aula 08 - variaveis Compostas/src/vetores/Vetor.java
+++ Aula 08 - variaveis Compostas/src/vetores/Vetor.java
@@ -1,27 +1,27 @@
 package vetores;
 
-public class Vetores {
+public class Vetor {
 
 	public static void main(String[] args) {
 		
-		//Criando vetores
-		double[] notasAlunos = new double [3];
+		double[] notasAlunos = new double[10];
 		
-		//Atribuir valores para o vetor
+		//Atribuir valores ao Vetor
 		notasAlunos[0] = 10;
 		notasAlunos[1] = 8;
 		notasAlunos[2] = 6;
 		
-		//Ler os valores do vetor
 		System.out.println(notasAlunos[0]);
 		System.out.println(notasAlunos[1]);
 		System.out.println(notasAlunos[2]);
-
-		System.out.println("Exibindo os valores do vetor com for");
-		//Ler os valores do vetor - for
-		for (double nota : notasAlunos) {
+		
+		
+		System.out.println("Exibindo informações com o for");
+		//for
+		for(double nota : notasAlunos) {
 			System.out.println(nota);
 		}
+		
 		
 		
 	}
